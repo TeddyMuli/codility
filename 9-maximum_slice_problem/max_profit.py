69f345f395e8beb0e6f4de0ae84151fa35fb2087
TeddyMuli <tmuli974@gmail.com>
2025-07-21 17:31:28 +0300

    solve max_profit

--- 9-maximum_slice_problem/max_profit.py
+++ 9-maximum_slice_problem/max_profit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 def solution(A):
     min_price = float("inf")
     max_profit = 0
@@ -5,10 +6,11 @@ def solution(A):
     for price in A:
         if price < min_price:
             min_price = price
-        max_profit = price - min_price
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
 
     return max_profit
 
 if __name__ == "__main__":
-    test = []
+    test = [23171, 21011, 21123, 21366, 21013, 21367]
     print(solution(test))
