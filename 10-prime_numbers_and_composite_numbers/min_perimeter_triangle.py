ea002ea2df1dacfbef1455b6fb5ed86432595853
TeddyMuli <tmuli974@gmail.com>
2025-07-21 18:06:36 +0300

    solve min perimeter

--- 10-prime_numbers_and_composite_numbers/min_perimeter_triangle.py
+++ 10-prime_numbers_and_composite_numbers/min_perimeter_triangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def solution(N):
-    for i in range(int(N**0.5), 1, -1):
+    for i in range(int(N**0.5), 0, -1):
         if N % i == 0:
             sideA = i
             break
@@ -12,4 +12,5 @@ def solution(N):
 
 if __name__ == "__main__":
     test = 30
-    print(solution(test))
+    test2 = 78
+    print(solution(1))
