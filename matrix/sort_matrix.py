d564214d7222c4e50ba36b162f8aeb45d9d37eba
TeddyMuli <tmuli974@gmail.com>
2025-07-28 16:46:48 +0300

    solve sort matrix

---
+++ matrix/sort_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Solution:
+    def sortedMatrix(self,N,Mat):
+        elements = [Mat[i][j] for i in range(N) for j in range(N)]
+
+        elements.sort()
+
+        k=0
+        for i in range(N):
+            for j in range(N):
+                Mat[i][j] = elements[k]
+                k+=1
+
+        return Mat
+
+if __name__ == "__main__":
+    solution = Solution()
+    test=[[10,20,30,40],
+        [15,25,35,45],
+        [27,29,37,48],
+        [32,33,39,50]]
+    print(solution.sortedMatrix(4, test))
