de4cf48516b1ca45ac70442ab5d1493f3933a549
TeddyMuli <tmuli974@gmail.com>
2025-08-05 17:44:15 +0300

    solve longest consecutive number

---
+++ leetcode/128-longest_consecutive/solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class Solution(object):
+    def longestConsecutive(self, nums):
+        if len(nums) < 1:
+            return 0
+        nums.sort()
+        max_count = 0
+        current_count = 1
+        first = nums[0]
+
+        for num in nums[1:]:
+            if num == first + 1:
+                current_count += 1
+            else:
+                if num != first:
+                    max_count = max(current_count, max_count)
+                    current_count = 1
+            first = num
+
+        max_count = max(current_count, max_count)
+
+        return max_count
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1,0,1,2]
+    print(sol.longestConsecutive(nums))
