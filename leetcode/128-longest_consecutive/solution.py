d5e98dd60057d86b8955782306b6c0f85ca7c295
TeddyMuli <tmuli974@gmail.com>
2025-08-05 19:27:35 +0300

    fix find longest consecutive numbers

--- leetcode/128-longest_consecutive/solution.py
+++ leetcode/128-longest_consecutive/solution.py
@@ -3,22 +3,18 @@ class Solution(object):
     def longestConsecutive(self, nums):
         if len(nums) < 1:
             return 0
-        nums = sorted(set(nums))
-        max_count = 0
-        current_count = 1
-        first = nums[0]
+        
+        best = 0
+        nums = set(nums)
 
-        for num in nums[1:]:
-            if num == first + 1:
-                current_count += 1
-            else:
-                max_count = max(current_count, max_count)
-                current_count = 1
-            first = num
+        for num in nums:
+            if num - 1 not in nums:
+                next = num + 1
+                while next in nums:
+                    next += 1
+                best = max(best, next - num)
 
-        max_count = max(current_count, max_count)
-
-        return max_count
+        return best
 
 if __name__ == "__main__":
     sol = Solution()
