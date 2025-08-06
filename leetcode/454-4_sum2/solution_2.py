0417c716b57f05becacfee98ef30dc37732b489d
TeddyMuli <tmuli974@gmail.com>
2025-08-06 17:40:11 +0300

    implement even better solution O(n^2)

--- leetcode/454-4_sum2/solution_2.py
+++ leetcode/454-4_sum2/solution_2.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+from collections import Counter
+class Solution:
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        AB = Counter(a + b for a in nums1 for b in nums2)
+        return sum(AB[-c-d] for c in nums3 for d in nums4)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums1 = [1,2]
+    nums2 = [-2,-1]
+    nums3 = [-1,2]
+    nums4 = [0,2]
+    print(sol.fourSumCount(nums1, nums2, nums3, nums4))
