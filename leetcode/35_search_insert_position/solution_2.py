5beeda945c6b671d9157281d1fe19fd963e39fff
TeddyMuli <tmuli974@gmail.com>
2025-08-27 15:32:26 +0300

    implement solution in O(logn) using binary search

--- leetcode/35_search_insert_position/solution_2.py
+++ leetcode/35_search_insert_position/solution_2.py
@@ -1,9 +1,20 @@
 class Solution:
     def searchInsert(self, nums, target):
-        pass
+        # use binary search
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
 
 if __name__ == "__main__":
     nums = [1,3,5,6]
-    target = 5
+    target = 7
     sol = Solution()
     print(sol.searchInsert(nums, target))
